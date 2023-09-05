b142f61e25136a9ecd062cd95ac9c72ea4f68824
MemoiVentur3 <141185642+MemoiVentur3@users.noreply.github.com>
2023-09-05 16:16:11 +0000

    upload

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,10 +10,10 @@ const About = () => {
     <div className='bg-white w-full' id='About'>
         <div className='flex items-center flex-col p-8 '>
             <div className='flex flex-col items-center justify-center py-8 lg:w-[80%]'>
-                <Fade top><h1 className='font-extrabold lg:text-6xl md:text-5xl text-4xl text-[#130395]'>About</h1></Fade>
-                <Fade bottom><div className='font-extrabold  lg:text-6xl md:text-5xl text-4xl text-[#130395] flex items-center gap-2'> <h1>Memoi</h1> <h1>Africa</h1></div></Fade>
+                <Fade top><h1 className='font-extrabold lg:text-6xl md:text-5xl text-4xl text-[#130395]'>Who we Are</h1></Fade>
+                {/* <Fade bottom><div className='font-extrabold  lg:text-6xl md:text-5xl text-4xl text-[#130395] flex items-center gap-2'> <h1>Memoi</h1> <h1>Africa</h1></div></Fade> */}
                 <div className=' text-center mt-3 text-[#130395]'>
-                    <p className='ease-in duration-300 text-[#130395] text-sm font-semibold '>At MEMOI AFRICA, we are more than just a community – we are a catalyst for change, a driving force that propels African innovation to new heights. Our platform serves as a nurturing ground for tech enthusiasts, developers, and visionary founders to converge, collaborate, and cultivate their ideas. With a strong emphasis on Blockchain, AI, and other emerging technologies, we believe in harnessing the immense potential of these domains to redefine industries, uplift economies, and solve pressing societal challenges. Rooted in the rich tapestry of Africa's diverse cultures and experiences, MEMOI AFRICA celebrates the continent's unique perspective while fostering a global outlook. Our mission is to empower, educate, and elevate individuals and businesses, creating a ripple effect that resonates far beyond the boundaries of technology. As we unite innovation with passion, MEMOI AFRICA stands as a beacon of progress, poised to leave an indelible legacy in the ever-evolving landscape of tech innovation.</p>
+                    <p className='ease-in duration-300 text-[#130395] text-md font-semibold '>At Memoi, we believe that Africa is the next frontier and can play more active roles in shaping the future of tech by creating solutions to real world problems. Therefore, we are bridging the gap between African developers and the rest of the world.</p>
                 </div>
             </div>
             {/* images */} 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,10 +24,12 @@ const Hero = () => {
                     <div className='flex flex-col gap-3 text-white lg:items-start items-center '>
                         <Fade top><h1 className='font-extrabold lg:text-9xl md:text-6xl text-6xl'>MEMOI</h1></Fade>
                         <Fade bottom><h1 className='font-extrabold lg:text-9xl md:text-6xl text-6xl '>AFRICA</h1></Fade>
-                        <LightSpeed top><p className='ease-in duration-300 lg:text-left text-center text-gray-300 text-sm'>Welcome to MEMOI AFRICA – a vibrant hub for African developers and founders at the forefront of technological innovation. Our community is a dynamic melting pot of creativity, collaboration, and expertise, driven by a shared passion for driving progress in the realms of Blockchain, AI, and other emerging technologies. </p></LightSpeed>
+                        <LightSpeed top><p className='ease-in duration-300 lg:text-left text-center text-gray-300 text-sm'>Memoi is a community of African developers and founders.
+                            Our primary mission is to fuel growth in the areas of Blockchain, AI, IoT and other emerging techs. We empower young builders to create practical solutions to real-world challenges and make global impact.
+                        </p></LightSpeed>
                         {/* button */}
                         <div className='grid lg:grid-cols-2 gap-2 w-full'>
-                            <Button variant='outlined hero-btn'><Link to='About'>Developers<ArrowForward /> </Link></Button>
+                            <Button variant='outlined hero-btn'><Link to='https://nas.io/memoi-africa'>Developers<ArrowForward /> </Link></Button>
                             <Button variant='outlined hero-btn'><Link to='About'>Partners <ArrowForward /> </Link></Button>
                         </div>
                         {/* image buttons */}
@@ -37,7 +39,7 @@ const Hero = () => {
                         </div> */}
                     </div>
                     {/* hero image */}
-                    <Zoom  right>
+                    <Zoom right>
                         <div className='hidden lg:block  hero-img-holder h-[450px] w-[450px]'></div>
                         {/* <img className='rounded-xl hidden lg:block w-[100%] ' src={img} alt="" /> */}
                     </Zoom>
@@ -46,22 +48,67 @@ const Hero = () => {
             {/* progress counter */}
             <div className='flex items-center justify-evenly mt-1 py-10 w-full bg-[#130395] text-white lg:px-8 px-2'>
                 <div className='flex flex-col gap-2 items-center'>
-                    <h1 className='font-bold lg:text-[60px] text-4xl text-white'>5,000+</h1>
+                    <h1 className='font-bold lg:text-[60px] text-md md:text-4xl text-white'>5,000+</h1>
                     <p className='lg:text-lg text-xs flex lg:flex-row flex-col gap-1'><span>Developer</span> <span>Community</span></p>
                     {/* progress bar */}
                     <div className='service-progress rounded-full lg:block hidden'></div>
                 </div>
                 <div className='flex flex-col gap-2 items-center'>
-                <h1 className='font-bold lg:text-[60px] text-4xl text-white'>40+</h1>
-                <p className='lg:text-lg text-xs flex lg:flex-row flex-col gap-1'><span>University</span> <span>Partners</span></p>
+                    <h1 className='font-bold lg:text-[60px] text-md md:text-4xl text-white'>40+</h1>
+                    <p className='lg:text-lg text-xs flex lg:flex-row flex-col gap-1'><span>University</span> <span>Partners</span></p>
                     {/* progress bar */}
                     <div className='service-progress rounded-full lg:block hidden'></div>
                 </div>
                 <div className='flex flex-col gap-2 items-center'>
-                    <h1 className='font-bold lg:text-[60px] text-4xl text-white'>100%</h1>
+                    <h1 className='font-bold lg:text-[60px] text-md md:text-4xl text-white'>100%</h1>
                     <p className='lg:text-lg text-xs flex lg:flex-row flex-col gap-1'><span>Project</span> <span>Success</span></p>
                     {/* progress bar */}
                     <div className='bootcamp-progress rounded-full lg:block hidden'></div>
+                </div>
+            </div>
+
+            <div className="bg-[#130395] py-24 sm:py-32">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <h2 className="text-center text-lg font-semibold leading-8 text-white">
+                        Trusted by the world’s most innovative teams
+                    </h2>
+                    <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+                        <img
+                            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                            src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
+                            alt="Transistor"
+                            width={158}
+                            height={48}
+                        />
+                        <img
+                            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                            src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
+                            alt="Reform"
+                            width={158}
+                            height={48}
+                        />
+                        <img
+                            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                            src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
+                            alt="Tuple"
+                            width={158}
+                            height={48}
+                        />
+                        <img
+                            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+                            src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
+                            alt="SavvyCal"
+                            width={158}
+                            height={48}
+                        />
+                        <img
+                            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+                            src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
+                            alt="Statamic"
+                            width={158}
+                            height={48}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
