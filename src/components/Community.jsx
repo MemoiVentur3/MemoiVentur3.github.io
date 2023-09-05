e77bc92f6f440caf739981028e0750e593917381
MemoiVentur3 <141185642+MemoiVentur3@users.noreply.github.com>
2023-09-05 16:55:21 +0000

    updated community contents

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -13,8 +13,10 @@ const Community = () => {
                 <div className='flex flex-col gap-3 items-center justify-center py-8 lg:w-[80%]'>
                     <img src={img1} className='img h-[400px] lg:w-[80%] lg:mb-16 rounded-3xl' alt="" />
                     <h1 className='font-extrabold  lg:text-6xl md:text-5xl text-4xl'>Community</h1>
-                    <p className='text-center ease-in duration-300  text-white text-sm'>At MEMOI AFRICA, our community is the beating heart of our mission. We are a collective of forward-thinkers, trailblazers, and problem solvers who understand the power of collaboration. When you join our community, you're not just signing up – you're stepping into an ecosystem of knowledge-sharing, skill-building, and limitless possibilities. Whether you're a seasoned developer or a budding entrepreneur, our inclusive platform welcomes you with open arms. Through engaging events, interactive workshops, and spirited discussions, we foster an environment where ideas flourish and connections thrive. As you join the community via button beneath here, isn't just an invitation; it's a commitment to being a part of something greater than yourself. Together, we amplify each other's strengths, navigate challenges, and collectively shape the trajectory of African tech innovation. Embark on this journey with us, and let's co-create a future that's not only promising but transformative.</p>
+                    <p className='text-center ease-in duration-300  text-white text-sm'>We are building a community of 100,000 developers in Africa who want to learn, build, and get jobs. Join our network of developers & hack to win prizes, explore courses, get access to jobs and life-changing opportunities.</p>
+                    <a href='hhtps://nas.io/memoi-africa'>
                     <img src={communityBtn} className='cursor-pointer w-[250px]' alt="" />
+                    </a>
                     {/* <Button variant='contained' className='contained-lg-btn '>Join Community</Button> */}
                 </div>
             </div>
